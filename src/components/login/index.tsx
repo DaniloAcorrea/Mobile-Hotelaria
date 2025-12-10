3773b635cd95627d29a460def9ba8508458bcf0d
Pamella Pereto <pamellapereto@gmail.com>
2025-12-10 15:36:12 -0300

    Validações de dados da tela login

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,12 +1,36 @@
 import { useRouter } from "expo-router";
+import React, { useMemo, useState } from 'react';
 import { Dimensions, Text, TouchableOpacity, View } from "react-native";
 import AuthContainer from "../ui/AuthContainer";
 import PasswordField from "../ui/PasswordField";
 import TextField from "../ui/TextField";
 import { global } from "../ui/styles";
 
+function isValidEmail(email: string) {
+  return /^[^\s@&='"!]@[^\s@&='"!].[^\s@&='"!]$/.test(email);
+}
 const RenderLogin = () => {
   const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [touched, setTouched] = useState<{email?: boolean; password?: boolean}>({});
+
+  const errors = useMemo(() => {
+      const error: Record<string, string> = {};
+      if (touched.email && !email) error.email = "E-mail obrigatório";
+      if (touched.password && !password) error.password = "Senha obrigatória";
+      if (touched.password && password && password.length < 6) error.password = "No mínimo 6 caracteres para a senha";
+      if (touched.email && email && !isValidEmail(email)) error.email = "Digite um e-mail válido";
+      return error;
+  }, [email, password, touched]);
+  const canSubmit = email && password && Object.keys(errors).length === 0 && !loading;
+
+  //Back-end
+  const handleSubmit = async () => {
+    router.replace("/(tabs)/explorer");
+    
+  }
 
   const { width, height } = Dimensions.get("window");
   return (
@@ -21,6 +45,9 @@ const RenderLogin = () => {
           label="E-mail"
           icon={{ lib: "MaterialIcons", name: "email" }}
           placeholder="user@email.com"
+          value={email}
+          onChangeText={(input) => setEmail(input)}
+          errorText={errors.email}
           keyboardType="email-address"
         />
 
@@ -28,10 +55,14 @@ const RenderLogin = () => {
           label="Senha"
           icon={{ lib: "MaterialIcons", name: "lock" }}
           placeholder="*********"
+          value={password}
+          onChangeText={(input) => setPassword(input)}
+          errorText={errors.password}
         />
         <TouchableOpacity
           style={[global.primaryButton]}
-          onPress={() => router.push("/(tabs)/explorer")}
+          onPress={handleSubmit}
+          disabled={!canSubmit}
         >
           <Text style={global.primaryButtonText}>Entrar</Text>
         </TouchableOpacity>
